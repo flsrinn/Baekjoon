52d235b5cb0e9a0e314e9c4c97fd80fb511529cd
Ahrin Jun <wjsdkfls03@naver.com>
2025-03-23 22:47:00 +0900

    [level 2] Title: H-Index, Time: 4.76 ms, Memory: 92.9 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,16 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
-        
-        Integer[] sorted = Arrays.stream(citations).boxed().toArray(Integer[]::new);
-        Arrays.sort(sorted, Comparator.reverseOrder());
-        
-        for(int i=0; i<sorted.length; i++) {
-            if(sorted[i] >= i+1) answer++;
-        }
-        
         return answer;
     }
 }
